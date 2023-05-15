abd863bb0d6274bfc1c12bf972d3bbef62a69921
YBringui <ybringui@gmail.com>
2023-05-15 12:08:46 +0200

    Transfert

---
+++ C08/ex00/ft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybringui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 17:47:43 by ybringui          #+#    #+#             */
+/*   Updated: 2022/12/21 21:57:31 by ybringui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT.H
+
+# define FT.H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
